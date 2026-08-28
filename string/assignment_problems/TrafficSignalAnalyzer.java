fbf652e88a023236cfbba4c89c4e80353822f7ad
rshreyamenon-10 <rshreyamenon@gmail.com>
2026-08-29 00:06:35 +0530

    Question 3

---
+++ string/assignment_problems/TrafficSignalAnalyzer.java
@@ -0,0 +1,36 @@
+public class TrafficSignalAnalyzer {
+
+    public static void findLongestStreak(String signalLog) {
+        if (signalLog == null || signalLog.length() == 0) {
+            System.out.println("No signals recorded.");
+            return;
+        }
+
+        char maxColor = signalLog.charAt(0);
+        int maxStreak = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int currentStreak = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+            if (signalLog.charAt(i) == currentColor) {
+                currentStreak++;
+            } else {
+                currentColor = signalLog.charAt(i);
+                currentStreak = 1;
+            }
+
+            if (currentStreak > maxStreak) {
+                maxStreak = currentStreak;
+                maxColor = currentColor;
+            }
+        }
+
+        System.out.println("Longest Streak: '" + maxColor + "' repeated " + maxStreak + " times");
+    }
+
+    public static void main(String[] args) {
+        findLongestStreak("RRGGGYRR");
+        findLongestStreak("RRRRYYGG");
+    }
+}
