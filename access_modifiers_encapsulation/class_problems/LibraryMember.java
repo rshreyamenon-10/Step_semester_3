498efb0ce2865afdf4c69e33e369bf727dce92a4
rshreyamenon-10 <rshreyamenon@gmail.com>
2026-09-25 19:58:06 +0530

    Question 4

---
+++ access_modifiers_encapsulation/class_problems/LibraryMember.java
@@ -0,0 +1,51 @@
+public class LibraryMember {
+
+    private String membershipId;
+    private String name;
+    private boolean premiumMember;
+    private String securityAnswer;
+
+    public LibraryMember() {
+        this(null, null);
+    }
+
+    public LibraryMember(String name) {
+        this(null, name);
+    }
+
+    public LibraryMember(String membershipId, String name) {
+        this.membershipId = membershipId;
+        this.name = name;
+        this.premiumMember = false;
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String id) {
+        if (this.membershipId == null) {
+            this.membershipId = id;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremiumMember() {
+        return premiumMember;
+    }
+
+    public void setPremiumMember(boolean premium) {
+        this.premiumMember = premium;
+    }
+
+    public void setSecurityAnswer(String answer) {
+        this.securityAnswer = answer;
+    }
+}
